3f93c55da199233b6511c3c2d8fb430c1f0594a6
valya <user@mail.ru>
2022-09-06 20:01:03 +0300

    add Homework_1

---
+++ Homework_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+ 
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine ("Введите второе число: ");
+            int num2 = Convert.ToInt32 (Console.ReadLine());
+ 
+            if (num1 > num2)
+            {
+                Console.WriteLine("Первое число больше второго");
+            }
+            else if (num1 < num2)
+            {
+                Console.WriteLine("Первое число меньше второго");
+                
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
